cdfdd4dff7507686d044caf56f80e63ffde35c28
venki4meu <62737401+venki4meu@users.noreply.github.com>
2020-04-29 21:04:24 +0530

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace Docker_CoreApi.Controllers
         private static readonly string[] Summaries = new[]
         {
             "Freezing",
-            // "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            // "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching1"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
